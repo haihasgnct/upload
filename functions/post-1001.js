b8b63068c86956daa37b8c2882ca949b8cf2ce2a
haihasgnct <54738724+haihasgnct@users.noreply.github.com>
2022-03-22 15:44:32 +0700

    Update post-1001.js

--- functions/post-1001.js
+++ functions/post-1001.js
@@ -1,10 +1,10 @@
 exports.handler = async event => {
-  if (event.queryStringParameters.fb) {
+  if (event.queryStringParameters.fbclid) {
     return {
       statusCode: 301,
       headers: {
         'cache-control': 'public, max-age=0, must-revalidate',
-        location: 'https://superpetstv.com/dog-cries-with-joy-when-r%e1%b4%87%e1%b4%9c%c9%b4%c9%aa%e1%b4%9b%e1%b4%87%e1%b4%85-with-c%e1%b4%8f%e1%b4%a1/'
+        location: 'https://www.google.com/'
       }
     }
   } else {
